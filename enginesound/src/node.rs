a3ee9940a07a7ea6ee54d9aac6603507d3fffb06
bendn <bend.n@outlook.com>
2023-04-13 09:14:49 +0700

    use a audiostreamgenerator as the base

--- enginesound/src/node.rs
+++ enginesound/src/node.rs
@@ -1,6 +1,8 @@
 use crate::gen::{Generator, LowPassFilter};
 use crate::utils::*;
-use godot::engine::{AudioStreamGeneratorPlayback, Engine, Node};
+use godot::engine::{
+    AudioStreamGenerator, AudioStreamGeneratorPlayback, AudioStreamGeneratorVirtual,
+};
 use godot::prelude::*;
 const DEFAULT_CONFIG: &[u8] = include_bytes!("default.esc");
 
@@ -8,29 +10,28 @@ type Stream = Gd<AudioStreamGeneratorPlayback>;
 
 /// Procedural engine sound generation
 #[derive(GodotClass)]
-#[class(base=Node)]
+#[class(base=AudioStreamGenerator)]
 #[allow(dead_code)]
-pub struct EngineNoise {
+pub struct EngineStream {
     generator: Option<Generator>,
     stream: Option<Stream>,
     #[export(get, set)]
-    rpm: f32,
+    engine_rpm: f32,
     #[export(get, set)]
-    volume: f32,
+    engine_volume: f32,
     #[base]
-    base: Base<Node>,
+    base: Base<AudioStreamGenerator>,
 }
-const SAMPLE_RATE: u32 = 30000;
 
 #[godot_api]
-impl NodeVirtual for EngineNoise {
-    fn init(base: Base<Node>) -> Self {
+impl AudioStreamGeneratorVirtual for EngineStream {
+    fn init(base: Base<AudioStreamGenerator>) -> Self {
         Self {
             generator: None,
             base,
-            rpm: 883.0,
-            volume: 1.0,
             stream: None,
+            engine_rpm: 883.0,
+            engine_volume: 1.0,
         }
     }
 }
@@ -50,45 +51,38 @@ macro_rules! fail_cond {
     };
 }
 
-macro_rules! require_runtime_some {
-    ($retval: expr, $opt: expr) => {
-        if Engine::singleton().is_editor_hint() {
-            return $retval;
-        }
-        fail_cond!($opt.is_none(), "Variable unset.", $retval);
-    };
-}
-
 #[godot_api]
-impl EngineNoise {
+impl EngineStream {
+    /// Creates a generator.
     #[func]
     fn make_engine(&mut self) {
         let mut engine = ron::de::from_bytes(DEFAULT_CONFIG).expect("default config is invalid");
-        fix_engine(&mut engine, SAMPLE_RATE);
-        let mut generator =
-            Generator::new(SAMPLE_RATE, engine, LowPassFilter::new(0.5, SAMPLE_RATE));
+        fix_engine(&mut engine, self.get_mix_rate() as u32);
+        let mut generator = Generator::new(
+            self.get_mix_rate() as u32,
+            engine,
+            LowPassFilter::new(0.5, self.get_mix_rate() as u32),
+        );
         generator.volume = 1.0;
         self.generator = Some(generator)
     }
 
+    /// Fills the [AudioStreamGeneratorPlayback]'s buffer.
     #[func]
     fn update(&mut self) {
         fail_cond!(self.stream.is_none(), "No stream!");
-        require_runtime_some!((), self.generator);
+        if self.generator.is_none() {
+            self.make_engine();
+        }
         let gen = self.generator.as_mut().unwrap();
-        gen.volume = self.volume;
-        gen.engine.rpm = self.rpm;
-        gen.generate(&mut self.stream.as_mut().unwrap());
+        gen.volume = self.engine_volume;
+        gen.engine.rpm = self.engine_rpm;
+        gen.generate(self.stream.as_mut().unwrap());
     }
 
+    /// Sets the [AudioStreamGeneratorPlayback] for this engine.
     #[func]
     fn set_stream(&mut self, stream: Stream) {
         self.stream = Some(stream);
-        self.update();
-    }
-
-    #[func]
-    fn set_volume(&mut self, v: f32) {
-        self.generator.volume = v
     }
 }
--- test.gd
+++ test.gd
@@ -1,19 +1,18 @@
-extends EngineNoise
+extends AudioStreamPlayer
 
-@export var player: AudioStreamPlayer;
+@onready var strem: EngineStream = stream
 
 func _ready() -> void:
 	set_process(false)
-	player.play()
-	make_engine()
-	set_stream(player.get_stream_playback())
+	play()
+	strem.set_stream(get_stream_playback())
 	set_process(true)
 
 func _process(_d: float):
-	update()
+	strem.update()
 
 func _on_vol_value_changed(value: float) -> void:
-	volume = value
+	strem.engine_volume = value
 
 func _on_rpm_value_changed(value: float) -> void:
-	rpm = value
+	strem.engine_rpm = value
